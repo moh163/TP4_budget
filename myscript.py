3a0f8b6e38dcd3e894f8019fdda0905379e200a3
moh163 <97916160+moh163@users.noreply.github.com>
2023-11-14 15:32:48 -0500

    myscript.py final

--- myscript.py
+++ myscript.py
@@ -1,5 +1,5 @@
 import os
-os.system('goodhash=e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c ; badhash=c1a4be04b972b6c17db242fc37752ad517c29402')
-os.system('git bisect start $badhash $goodhash')
+os.system('goodhash=e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c ; badhash=c1a4be04b972b6c17db242fc37752ad517c29402 ; git bisect start $badhash $goodhash')
 os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
+
