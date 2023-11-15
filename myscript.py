58922f81893d79f5876d1d6b8c3bdcb9e5adcb6f
Yanis <90219842+SkyZonGithub@users.noreply.github.com>
2023-11-14 19:05:43 -0500

    myscript

--- myscript.py
+++ myscript.py
@@ -1,5 +1,8 @@
 import os
-os.system('goodhash=e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c ; badhash=c1a4be04b972b6c17db242fc37752ad517c29402 ; git bisect start $badhash $goodhash')
+
+badHash='c1a4be04b972b6c17db242fc37752ad517c29402'
+goodHash='e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c'
+
+os.system('git bisect start %s %s' % (badHash, goodHash))
 os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
-
